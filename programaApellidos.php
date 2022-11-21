b79b1192de7521c8b58110ba5a10fe75e283e50c
diazfabrici0 <fabricio.diaz@est.fi.uncoma.edu.ar>
2022-11-21 01:27:21 -0300

    Modificaciones en wordix.php y programaApellidos

--- programaApellidos.php
+++ programaApellidos.php
@@ -1,46 +1,35 @@
 <?php
 include_once("wordix.php");
 
-/**************************************/
-/***** DATOS DE LOS INTEGRANTES *******/
-/**************************************/
-
 /* 
 Diaz, Fabricio. FAI-4210. TUWD. dfabricio255@gmail.com. diazfabrici0
 Ferrada, Bruno. FAI-3307. TUDW. brunoferrada1212@gmail.com. BrunoFerrada
 Ferrada, Mauro. FAI-4211. TUDW. mauroferrada01@gmail.com. MauroFerrada
 Lantaño, Daniel Ariel. FAI-2305. TUDW. s.lantanoariel@gmail.com. lantanoariel
 */
-/* ... COMPLETAR ... */
-
-
 
 /**************************************/
 /***** DEFINICION DE FUNCIONES ********/
 /**************************************/
 
-/**
- * Obtiene una colección de palabras
- * @return array
+/** Muestra el menú para el usuario
+ * @param string $player
  */
-function cargarColeccionPalabras()
-{
-    $coleccionPalabras = [
-        "MUJER", "QUESO", "FUEGO", "CASAS", "RASGO",
-        "GATOS", "GOTAS", "HUEVO", "TINTO", "NAVES",
-        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS", 
-        "PERRO", "PLAZA", "DANZA", "LAPIZ", "RELOJ"
-
-    ];
-
-    return ($coleccionPalabras);
+function mostrarMenu ($player){
+    //$opcionElegida
+    escribirMensajeBienvenida($player);
+    echo "\n";
+    echo "***************************************************** \n";
+    echo "1) Jugar Wordix con una palabra predeterminada \n";
+    echo "2) Jugar Wordix con una palabra aleatoria \n";
+    echo "3) Mostrar una partida \n";
+    echo "4) Mostrar la primera partida ganadora \n";
+    echo "5) Mostrar resumen de un jugador \n";
+    echo "6) Mostrar listado de highscore \n";
+    echo "7) Agregar una palabra de 5 letras \n";
+    echo "8) Salir \n";
+    echo "***************************************************** \n";
 }
-
-function palabraElegida()
- {
-    
-}
-
 /* ... COMPLETAR ... */
 
 
@@ -53,41 +42,29 @@ function palabraElegida()
 
 /*
 string $jugador
-int $opcionElegida, $numPalabra
+int $opcionElegida, $numPalabra, $elMax, $elMin
 
 */
 
 
 
 //INICIALIZACION DE VARIABLES:
-
+$miColeccionPalabras = [];
+$miColeccionPalabras=cargarColeccionPalabras($miColeccionPalabras);
 
 
 //PROCESO:
 
-echo " Bienvenido! Por favor ingrese su nombre. \n";
-$jugador = trim(fgets(STDIN));
-escribirMensajeBienvenida($jugador);
-echo "\n";
-echo "******************************************************** \n";
-echo "*Elija una opcion*\n";
-echo "1) Jugar Wordix con una palabra predeterminada \n";
-echo "2) Jugar Wordix con una palabra aleatoria \n";
-echo "3) Mostrar una partida \n";
-echo "4) Mostrar la primera partida ganadora \n";
-echo "5) Mostrar resumen de un jugador \n";
-echo "6) Mostrar listado de highscore \n";
-echo "7) Agregar una palabra de 5 letras \n";
-echo "8) Salir \n";
-echo "********************************************************";
-$opcionElegida = trim(fgets(STDIN));
-
-
 //print_r($partida);
 //imprimirResultado($partida);
 
+echo " Bienvenido! Por favor ingrese su nombre. \n";
+$jugador = trim(fgets(STDIN));
+echo mostrarMenu($jugador);
 
+echo "*Elija una opcion*\n";
 
+$opcionElegida = trim(fgets(STDIN));
 
 do {
    
@@ -96,8 +73,10 @@ do {
     switch ($opcionElegida) {
         case 1: 
             if($opcionElegida == 1){
-                echo "Ingrese un el número de palabra \n";
-                $numPalabra = trim(fgets(STDIN));
+                $elMax = miMaxInd($miColeccionPalabras);
+                $elMin = miMenInd($elMax);
+                $numPalabra=solicitarNumeroEntre($elMin, $elMax);
+                echo $numPalabra;
             }
 
             break;
--- wordix.php
+++ wordix.php
@@ -29,23 +29,64 @@ const ESTADO_LETRA_PERTENECE = "pertenece";
 /**
  *  ****COMPLETAR*****
  */
+
+
+/**
+ * Obtiene una colección de palabras
+ * @return array
+ */
+function cargarColeccionPalabras()
+{
+    $coleccionPalabras = [
+        "MUJER", "QUESO", "FUEGO", "CASAS", "RASGO",
+        "GATOS", "GOTAS", "HUEVO", "TINTO", "NAVES",
+        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS", 
+        "PERRO", "PLAZA", "DANZA", "LAPIZ", "RELOJ"
+        ];
+
+    return ($coleccionPalabras);
+}
+
+
+
+/** Determina el indice maximo de un array
+ * @param array $coleccionPal
+ * @return int
+ */
+function miMaxInd($coleccionPal)
+{
+    //int $maxInd
+    $maxInd = count($coleccionPal)-1;
+    return ($maxInd);
+}
+
+/** Determina el indice Minimo de un array
+ * @param int $elMax
+ * @return int
+ */
+function miMenInd ($elMax){
+    //int $minInd
+    $minInd = $elMax - $elMax;
+    return ($minInd);
+}
+
+
+
+/**
+ * verifica si el valor es entero y si esta dentro del rango
+ * @param int $min
+ * @param int $max
+ * @return int
+ */
 function solicitarNumeroEntre($min, $max)
 {
     //int $numero
+    echo "ingrese el numero de la palabra: ";
     $numero = trim(fgets(STDIN));
-	
-	if (is_numeric($numero)) { //determina si un string es un número. puede ser float como entero.
-        $numero  = $numero * 1; //con esta operación convierto el string en número.
-    }
-	
-    while (!is_numeric($numero) ||( is_int($numero) && !($numero >= $min && $numero <= $max))) {
+    while (!is_integer($numero) && !($numero >= $min && $numero <= $max)) {
         echo "Debe ingresar un número entre " . $min . " y " . $max . ": ";
         $numero = trim(fgets(STDIN));
-		if (is_numeric($numero)) {
-			$numero  = $numero * 1; 
-		}
     }
-    
     return $numero;
 }
 
