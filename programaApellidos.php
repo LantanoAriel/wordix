b953505b220e0d5bd8f1cc7def5078d35abd83c6
lantanoariel <s.lantanoariel@gmail.com>
2022-11-24 13:20:36 -0300

    Cambios realizados en programaApellidos

--- programaApellidos.php
+++ programaApellidos.php
@@ -15,7 +15,8 @@ Lantaño, Daniel Ariel. FAI-2305. TUDW. s.lantanoariel@gmail.com. lantanoariel
 /** Muestra el menú para el usuario
  * @param string $player
  */
-function mostrarMenu (){
+function mostrarMenu()
+{
     //$opcionElegida
     echo "\n";
     echo "***************************************************** \n";
@@ -55,7 +56,7 @@ int $opcionElegida, $numPalabra, $elMax, $elMin, $nPartida
 $nPartida = 0;
 
 $miColeccionPalabras = [];
-$miColeccionPalabras=cargarColeccionPalabras($miColeccionPalabras);
+$miColeccionPalabras = cargarColeccionPalabras($miColeccionPalabras);
 $resumenGlobal = [];
 $partidasGanadas = [];
 //PROCESO:
@@ -69,86 +70,88 @@ do {
     echo "Seleccione una de las opciones: \n";
     $opcionElegida = trim(fgets(STDIN));
 
-    
+
     switch ($opcionElegida) {
-        case 1: 
+        case 1:
             echo " Bienvenido! Por favor ingrese su nombre. \n";
             $jugador = trim(fgets(STDIN));
-                $elMax = miMaxInd($miColeccionPalabras);
-                $elMin = miMenInd($elMax);
-                $numPalabra = solicitarNumeroEntre($elMin, $elMax);
-                //echo $numPalabra; //creo que este echo esta de mas -B
-                $palabraJuego = $miColeccionPalabras[$numPalabra];
-                $resultado = jugarWordix($palabraJuego, $jugador);
+            $elMax = miMaxInd($miColeccionPalabras);
+            $elMin = miMenInd($elMax);
+            $numPalabra = solicitarNumeroEntre($elMin, $elMax);
+            $palabraJuego = $miColeccionPalabras[$numPalabra];
+            $resultado = jugarWordix($palabraJuego, $jugador);
 
-                //hasta acá llega a jugar con la palabra elegida, igual hay un error en la ejecucion y se repite, no sé porqué -B
-            
+            //Aca ya te arreglé las cosas con los problemas que te repetian. -Ariel
+
 
             break;
-        case 2: 
-            
-                $conteo = count($miColeccionPalabras);
-                $aleatoria = mt_rand(0, $conteo - 1);
-                $palabraAleatoria = $miColeccionPalabras[$aleatoria];
-                $resultado = jugarWordix($palabraAleatoria, $jugador);
-                //llega a jugar con una palabra aleatoria, tiene el mismo error que el caso 1, se repite (y la funcion "mt_rand" la saque de internet) -B
-                //por las dudas no dejé declaradas la variables $conteo, $aleatoria y $resultado -B
-            
+        case 2:
+            echo " Bienvenido! Por favor ingrese su nombre. \n";
+            $jugador = trim(fgets(STDIN));
+            $conteo = count($miColeccionPalabras);
+            $aleatoria = mt_rand(0, $conteo - 1);
+            $palabraAleatoria = $miColeccionPalabras[$aleatoria];
+            $resultado = jugarWordix($palabraAleatoria, $jugador);
+            //llega a jugar con una palabra aleatoria, tiene el mismo error que el caso 1, se repite (y la funcion "mt_rand" la saque de internet) -B
+            //por las dudas no dejé declaradas la variables $conteo, $aleatoria y $resultado -BRUUUNO
+            //Ya te arreglé el tema de la repeticion infinita :) -ARIEL
+
             //jugar al wordix con una palabra aleatoria
+        
 
             break;
-        case 3: 
-            
-                $elMax = miMaxInd($coleccionPartidas);
-                $elMin = miMenInd($elMax);
-                do {
-                echo "seleccione una partida entre la". $elMin . "y la". $elMax;
+        case 3:
+
+            $elMax = miMaxInd($coleccionPartidas);
+            $elMin = miMenInd($elMax);
+            do {
+                echo "seleccione una partida entre la" . $elMin . "y la" . $elMax;
                 $nPartida = trim(fgets(STDIN));
-                echo "PARTIDA NÚMERO:". $nPartida;
+                echo "PARTIDA NÚMERO:" . $nPartida;
                 print_r($coleccionPartidas[$nPartida]);
                 echo "desea ver otra partida?(s/n)";
-                } while ($deNuevo == "si");
-            
+            } while ($deNuevo == "si");
+
             //mostrar una partida
 
             break;
         case 4:
-            
-                echo "PRIMERA PARTIDA GANADORA:";
-                print_r($partidasGanadas[0]);
-                
-            
+
+            echo "PRIMERA PARTIDA GANADORA:";
+            print_r($partidasGanadas[0]);
+
+
             //mostrar la primera partida ganadora
 
             break;
-        case 5: 
-            
-                do {
+        case 5:
+
+            do {
                 echo "escriba el nombre del jugador";
                 $nombreJ = trim(fgets(STDIN));
                 print_r($resumenGlobal[$nombreJ]);
                 echo "desea ver el resumen de otro jugador?(s/n)";
-                } while ($deNuevo == "si");
-            
+            } while ($deNuevo == "si");
+
             //mostrar resumen de jugador
 
             break;
         case 6:
-            
-                
-            
+
+
+
             //mostrar listado de partidas ordenados por jugador y por palabra
 
             break;
-        case 7: 
-            
-                echo "ingrese la palabra que quiera agregar a wordix:";
-                $nuevaPalabra = trim(fgets(STDIN));
-                $verificaPalabra = esPalabra($nuevaPalabra);
-                if ($verificaPalabra) {
-                    //añadir la palabra al array que aun no estoy seguro como se hace xd
-                }
-            
+        case 7:
+
+            echo "ingrese la palabra que quiera agregar a wordix:";
+            $nuevaPalabra = trim(fgets(STDIN));
+            $verificaPalabra = esPalabra($nuevaPalabra);
+            if ($verificaPalabra) {
+                //añadir la palabra al array que aun no estoy seguro como se hace xd
+            }
+
             //agregar una palabra de 5 letgras a wordix
 
             break;
@@ -156,6 +159,5 @@ do {
             //Salir
 
             break;
-
-  }} while ($opcionElegida != 8);
-    
+    }
+} while ($opcionElegida != 8);
